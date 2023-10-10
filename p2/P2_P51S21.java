d232dea4abae50f22d52d3d04308a73ed54775c7
Angel Aldair Cruz Medina <144179088+Aldair1218@users.noreply.github.com>
2023-10-10 15:40:50 -0600

    CRUZ MEDINA ANGEL ALDAIR

---
+++ p2/P2_P51S21.java
@@ -0,0 +1,27 @@
+public class p2p5_1s21{
+	public static void mai(string[]args){
+		int a=1;
+		while(a<10){
+			system,out,println("Estamos repitiendonos dentro del ciclo while");
+			a++;
+        }
+	 
+	    a=10;
+	    while(a<10){
+	    	system,out,println("Que pasa con esta instruccion");
+
+
+	    ;
+	    while(true){
+	    	system.out.println("aqui controlamos el contador de forma manual");
+	    	system.out.println("y generamos un if para que la condicion decida");
+	    	system.out.println("cuando debemos terminar la repeticion");
+	    	if (a>10) break;
+	    	a++;	
+	    }
+	    a=1
+        while(true){
+	    system.out.println("cual es la justificacion para no ejecutarse");
+	    }
+	}
+}
