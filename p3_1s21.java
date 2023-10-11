48e7f8bf402127aa36239e63a11b8766f0fe782d
Angel Aldair Cruz Medina <144179088+Aldair1218@users.noreply.github.com>
2023-10-11 12:37:09 -0600

    Delete p3_1s21.java

--- p3_1s21.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class P3_1s21{
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        String cadena;
-        System.out.println("Angel Aldair Cruz Medina");
-        cadena = teclado.nextline();
-    }
-}
