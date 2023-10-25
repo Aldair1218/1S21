fbafec6bed6415ac44298950732cb3fd3b961cce
Angel Aldair Cruz Medina <144179088+Aldair1218@users.noreply.github.com>
2023-10-25 12:56:43 -0600

    Add files via upload

---
+++ P2/P2p11_1s21.java
@@ -0,0 +1,13 @@
+public class P2p11_1s21{
+	public static void main(String[] args) {
+		for (String cadena : args){
+			char parametros = args[0].charAt(1);
+		switch(mensaje){
+		case 'a': System.out.println("Analisis de datos"); break;
+		case 'b': System.out.println("Analisis de datos"); break;
+		case 'h': System.out.println("Analisis de datos"); break;
+		case 'e': System.out.println("Analisis de datos"); break;
+	    }
+	  }
+	}
+}
